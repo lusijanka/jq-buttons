de80191b45d302a9dd2cb104eea98723acdd3119
lusijanka <lucjamiodek@gmail.com>
2017-08-16 15:24:39 +0200

    js errors corrected

--- jq.js
+++ jq.js
@@ -1,16 +1,16 @@
-$function() {
+$(function() {
 	var span = $('span');
 	span.each(function(index, element){
-		if(index % 2 == 0) {
+		if(index % 2 === 0) {
 			$(element).css('color', 'red');
 		};
 	});
 	var paragraphs = $('p');
 	paragraphs.each(function(index, element) {
-		var button = '<button class="btn" data-tmp=' + index + 'Click me</button>'
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
 		$(element).append(button)
 	});
 	$('button').click(function(){
-			alert($(this)).attr("data-tmp"));
+		alert($(this).attr('data-tmp'));
 	});
-};
+});
